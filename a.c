aa1969a810d85df5192af0f94745f00d404a9ba7
XUXIN-CHN <79997821@qq.com>
2018-01-04 17:20:45 +0800

    add new file a.c

---
+++ a.c
@@ -0,0 +1,21 @@
+/*************************************************************************
+    > File Name: a.c
+    > Author: XuXin
+    > Company:  
+    > Created Time: 2018年01月04日 星期四 17时19分46秒
+ ************************************************************************/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+int main(int argc,char *argv[])
+{
+    pritnf("hello Xavier\n");    
+    return 0;
+}
+
